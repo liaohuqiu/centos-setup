7f7ee84b82aaf722b185d0e81fc1b2ecad258928
liaohuqiu <liaohuqiu@gmail.com>
2013-09-13 14:09:47 -0400

    install php-fpm / ngnix

--- install/install_nginx.sh
+++ install/install_nginx.sh
@@ -5,7 +5,9 @@
 app=nginx-1.4.2
 url=http://nginx.org/download/nginx-1.4.2.tar.gz
 
-nginx_path=/usr/local/nginx
+current_dir=`pwd`
+nginx_path="/usr/local/nginx"
+sample_config_dir=$current_dir"/config/nginx"
 
 download_src $app $url
 goto_src $app
@@ -13,3 +15,14 @@ goto_src $app
 ./configure --prefix=$nginx_path
 make
 make install
+
+function make_easy_use()
+{
+    exe_cmd "cp  $sample_config_dir/init.d.nginx /etc/init.d/nginx"
+    chmod u+x /etc/init.d/nginx
+
+    chkconfig nginx on
+    service nginx start
+}
+
+make_easy_use
