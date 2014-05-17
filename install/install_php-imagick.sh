9aba8431fdc3d7bc4bf9c7fb2f242d484b90f857
liaohuqiu <liaohuqiu@gmail.com>
2014-05-17 10:33:54 +0800

    add imagick

--- install/install_php-imagick.sh
+++ install/install_php-imagick.sh
@@ -3,17 +3,17 @@
 current_dir=`pwd`
 . ../base.sh
 
-exe_cmd "sh install_libmemcached.sh"
+exe_cmd "sudo yum install ImageMagick-devel"
 
 exe_cmd "cd $current_dir"
-ext_name='memcached'
-file_key='memcached-2.2.0'
+ext_name='imagick'
+file_key='imagick-3.1.2'
 exe_cmd "sh fetch_source.sh $file_key"
 
 src_dir="/data0/install/src/$file_key"
 exe_cmd "cd $src_dir && ls -l"
 exe_cmd "phpize"
-exe_cmd "./configure --with-php-config=/usr/local/php/bin/php-config --with-libmemcached-dir=/usr/local/ --disable-memcached-sasl"
+exe_cmd "./configure"
 exe_cmd "make"
 
 exe_cmd "cd $current_dir"
