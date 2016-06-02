8af00452565588d604ca21a960830a029cc67fb3
liaohuqiu <liaohuqiu@gmail.com>
2016-06-02 10:17:39 +0800

    add files

--- remote-init.sh
+++ remote-init.sh
@@ -1,14 +1,23 @@
-if [ ! $# -eq 1 ]; then
-    echo "usage: $0 ssh-root-access"
+if [ ! $# -eq 2 ]; then
+    echo "usage: $0 user ssh-root-access-cmd"
     exit 1
 fi
 
+function exe_cmd()
+{
+    echo $1
+    eval $1
+}
+
+user=$1
+ssh_cmd=$2
 # 1. ssh to download script then run:
 #       init basic env
 #       add user / add sudo
 #       remove script
 
-cmd='wget https://raw.githubusercontent.com/liaohuqiu/centos-setup/master/add-user-as-root.sh | sh'
+cmd="curl https://raw.githubusercontent.com/liaohuqiu/centos-setup/master/add-user-without-passwd.sh | bash -s $user"
+exe_cmd "ssh_cmd '$cmd'"
 
 # 2. generate authkey then copy to remote
 # 3. login as new user, download script then 
