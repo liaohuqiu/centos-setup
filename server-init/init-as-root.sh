49cc9498abcf4fe3a41260a1fc93749833240b69
liaohuqiu <liaohuqiu@gmail.com>
2016-12-19 19:22:13 +0800

    add 11122

--- server-init/init-as-root.sh
+++ server-init/init-as-root.sh
@@ -16,10 +16,11 @@ hostname=$3
 function init_env() {
     exe_cmd "yum install git -y"
 
-    exe_cmd "firewall-cmd --zone=public --add-port=22/udp --permanent"
+    exe_cmd "firewall-cmd --zone=public --add-port=22/tdp --permanent"
     exe_cmd "firewall-cmd --zone=public --add-port=53/udp --permanent"
     exe_cmd "firewall-cmd --zone=public --add-port=80/tcp --permanent"
     exe_cmd "firewall-cmd --zone=public --add-port=443/tcp --permanent"
+    exe_cmd "firewall-cmd --zone=public --add-port=11122/tdp --permanent"
     exe_cmd "systemctl restart firewalld.service"
     exe_cmd "hostnamectl set-hostname $hostname"
 }
